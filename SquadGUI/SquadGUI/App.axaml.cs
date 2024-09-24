c6399c68b1cc67e4445fef6bf6c1092ded1fa9bc
MarcelDaBine <rafael_balan04@yahoo.com>
2024-09-24 03:43:21 +0200

    remade the loginpage similar to the qt ver

---
+++ SquadGUI/SquadGUI/App.axaml.cs
@@ -0,0 +1,40 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using SquadGUI.ViewModels;
+using SquadGUI.Views;
+
+namespace SquadGUI;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Line below is needed to remove Avalonia data validation.
+            // Without this line you will get duplicate validations from both Avalonia and CT
+            BindingPlugins.DataValidators.RemoveAt(0);
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new LoginView
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ SquadGUI/SquadGUI/Views/Dashboard.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SquadGUI.Views;
+
+public partial class Dashboard : UserControl
+{
+    public Dashboard()
+    {
+        InitializeComponent();
+    }
+}
